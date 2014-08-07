0cde6f5c2c30f09db34e009ac2e0bfd057748e33
Steve Baker <Steven.Baker@adazzle.com>
2014-08-07 16:49:18 +0100

    VERY basic example on how we could do editors

---
+++ examples/jsx/editableGrid.jsx
@@ -0,0 +1,79 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+var ExcelGrid = require('../../lib/ExcelGrid');
+var React = require('React');
+
+var EditableCell = React.createClass({
+  render: function() {
+    if(this.props.value.editing && this.props.isSelected) {
+      return (<span><input type="text" value={this.props.value.value} /></span>)
+    }
+    return (<span>{this.props.value.value}</span>)
+  }
+})
+
+var data = [];
+for (var i = 0; i < 2000; i++) {
+  data.push({
+    'key': i,
+    'supplier':{'value':'Supplier ' + i, 'editing':true},
+    'format': 'fmt ' + i,
+    'start':'start',
+    'end':'end',
+    'price':i });
+};
+
+
+function rows(start, end) {
+  return data.slice(start, end);
+}
+
+var columns = [
+
+  {
+    idx: 0,
+    name: 'Supplier',
+    key: 'supplier',
+    width: 300,
+    locked: true,
+    renderer: EditableCell
+  },
+  {
+    idx: 1,
+    name: 'Format',
+    key: 'format',
+    width: 350,
+  },
+  {
+    idx: 2,
+    name: 'Start',
+    key: 'start',
+    width: 250,
+  },
+  {
+    idx: 3,
+    name: 'End',
+    key: 'end',
+    width: 250,
+  },
+  {
+    idx: 4,
+    name: 'Cost',
+    key: 'cost',
+    width: 200,
+  }
+];
+
+
+var renderGrid = function(containerId) {
+  containerId = containerId || 'sandbox';
+  var grid = ExcelGrid({columns:columns, rows: rows, length: data.length, height: 400});
+    React.renderComponent(grid,
+      document.getElementById(containerId));
+};
+renderGrid();
+
+//force a global react object, for chrome dev tools if nothing else
+window.React = window.React || React;
