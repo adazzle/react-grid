4673b60fdabbe7e510f08f96ba6e2fb625753bbb
malonecj <malonecj@gmail.com>
2014-10-14 16:44:21 +0100

    remove standalone folder. This will be handled by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 var path = require("path");
 module.exports = {
 	entry: {
-		main : "./index",
-		withAddons : "./addons",
-    editors : "./editors"
+		main : "./standalone/index",
+		withAddons : "./standalone/addons"
 	},
 	output: {
-		path: path.join(__dirname, "js"),
+		path: path.join(__dirname, "./standalone/dist"),
 		filename: "reactGrid.[name].js",
 		library: ["ReactGrid", "[name]"],
 		libraryTarget: "umd"
