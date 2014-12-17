91ed4ee535fc0469c589922cd7ded2358993f67c
malonecj <malonecj@gmail.com>
2014-12-17 11:38:15 +0000

    some basic tests

--- test/Grid.spec.js
+++ test/Grid.spec.js
@@ -1,12 +1,50 @@
 'use strict';
 var React = require('react/addons');
+console.log("React is "  + React);
+var TestUtils = React.addons.TestUtils;
+
+var columns = [
+{
+  key: 'id',
+  name: 'ID',
+  width: '20%'
+},
+{
+  key: 'title',
+  name: 'Title'
+},
+{
+  key: 'count',
+  name: 'Count',
+  width: '20%'
+},
+]
+
+var getRows = function(start, end) {
+  var result = []
+  for (var i = start; i < end; i++) {
+    result.push({
+      id: i,
+      title: 'Title ' + i,
+      count: i * 1000
+    });
+  }
+  return result;
+}
 
 describe('Grid', () => {
   var Grid, component;
 
   beforeEach(() => {
-    Grid = require('../lib/Grid.js');
-    component= new Grid();
+    Grid = require('../lib/addons/grids/ExcelGrid.js');
+
+    console.log(Grid);
+    component = (<Grid
+    columns={columns}
+    length={1000}
+    rows={getRows(0, 1000)}/>);
+    debugger;
+    TestUtils.renderIntoDocument(component);
   });
 
   it('should create a new instance of Grid', () => {
