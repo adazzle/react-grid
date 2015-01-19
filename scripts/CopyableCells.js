19d9ddf825188ccad801ae072de0d8b520d5f379
Steve Baker <Steven.Baker@adazzle.com>
2015-01-19 22:35:25 +0000

    Updates

---
+++ scripts/CopyableCells.js
@@ -0,0 +1,73 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+var Grid = require('../../lib/addons/grids/CopyPasteGrid');
+var cx = React.addons.classSet;
+
+'use strict';
+
+var columns = [
+  {
+    key: 'id',
+    name: 'ID',
+    width: '20%'
+  },
+  {
+    key: 'title',
+    name: 'Title'
+  },
+  {
+    key: 'count',
+    name: 'Count',
+    width: '20%'
+  },
+]
+
+var getRows = function(start, end) {
+  var result = []
+  for (var i = start; i < end; i++) {
+    result.push({
+      id: i,
+      title: 'Title ' + i,
+      count: i * 1000
+    });
+  }
+  return result;
+}
+
+
+var Component = React.createClass({
+
+  getInitialState : function(){
+    return {rows : getRows(0, 1000)};
+  },
+
+  updateCell : function(commit){
+      var rows = this.state.rows;
+      var rowToChange = rows[commit.rowIdx];
+      if(rowToChange){
+        rowToChange[commit.cellKey] = commit.value;
+        this.setState({rows:rows});
+      }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <div className="well well-lg" >
+          <p>This shows a grid with copy/paste functionlity and keyboard navigation.</p>
+          <p>Hold Ctrl + C to copy a cells contents and Ctrl + V to paste</p>
+
+        </div>
+        <div>
+        <Grid
+        columns={columns}
+        length={1000}
+        rows={this.state.rows}
+        onCellChanged={this.updateCell} /></div>
+      </div>);
+  }
+});
+module.exports = Component;
---
+++ scripts/frozenCols.js
@@ -0,0 +1,58 @@
+/**
+ * @jsx React.DOM
+ */
+
+
+var Grid = require('../../lib/Grid');
+
+var React = require('react');
+
+function rows(start, end) {
+  var rows = [];
+  for (var i = start; i < end; i++) {
+    rows.push([i, 'Name ' + i, 'Surname ' + i]);
+  }
+  return rows;
+}
+
+var columns = [
+  {
+    name: 'No',
+    width: 300,
+    key: 0,
+    locked:true
+  },
+  {
+    name: 'Name',
+    width: 400,
+    resizeable: true,
+    key: 1
+  },
+  {
+    name: 'Surname',
+    width: 500,
+    resizeable: true,
+    key: 2
+  },
+];
+
+var Component = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="well well-lg" style={{width:"80%"}}>
+          <p>This shows a grid with fixed width columns and the first column frozen.</p>
+          <p>In this example, we set the width of the grids container div to 1000px, but have columns of 1200px, so you will always see the horizontal scrollbar.</p>
+          <p>Alternatively, leave off the width on your container and the grid will use 100% of the window width, meaning your scrollbar will depend on your screen dimensions</p>
+        </div>
+        <div style={{width:1000}}><Grid
+        columns={columns}
+        length={30000}
+        rows={rows}
+        rowHeight={40}/></div>
+      </div>);
+  }
+});
+
+
+module.exports = Component;
