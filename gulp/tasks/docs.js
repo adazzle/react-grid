ff2a0bc3977ad9e294b251c71c955ee06cc678c7
Steve Baker <Steven.Baker@adazzle.com>
2015-01-19 22:47:23 +0000

    simples... need to run examples to actually build the examples! still have the script folder issue, but shoudl e=be easy enough to fix that
    so - we now have http://adazzle.github.io/react-grid/

--- gulp/tasks/docs.js
+++ gulp/tasks/docs.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 var ghpages = require('gh-pages');
 var path = require('path');
-gulp.task('docs', ['build'], function (done) {
+gulp.task('docs', ['examples'], function (done) {
   ghpages.publish(path.join(__dirname, '../../examples'), function(err) { throw err; });
 });
