8646a702dfe39c92903e16cadc2dfb09eb8d35c2
malonecj <malonecj@gmail.com>
2015-01-23 10:44:39 +0000

    initial public site page

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -2,11 +2,11 @@ var browserSync = require('browser-sync');
 var gulp        = require('gulp');
 
 gulp.task('browserSync', ['examples'], function() {
-	
+
 	browserSync({
 		server: {
 			baseDir: './',
-    	index: "examples/index.html",
+    	index: "examples/index2.html",
 			routes: {
 				"/bower_components": "./bower_components",
 			}
