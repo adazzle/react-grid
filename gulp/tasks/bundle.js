8c3a9c9f70649d9a39f1dbdb9b9232de2d0449d2
malonecj <malonecj@gmail.com>
2015-02-26 12:12:41 +0000

    simplify gulp build process

---
+++ gulp/tasks/bundle.js
@@ -0,0 +1,33 @@
+var Webpack = require("webpack");
+var gutil     = require("gulp-util");
+
+var defaultErrorHandler = function(err, ctx) {
+//  if(err) throw new gutil.PluginError("webpack", err);
+
+    gutil.log("[webpack]", err);
+}
+module.exports = function (config, done, handlers) {
+  handlers = handlers || {};
+  handlers = {
+    onFatalError: handlers.onFatalError || defaultErrorHandler,
+    onError: handlers.onError || defaultErrorHandler,
+    onWarning: handlers.onWarning || defaultErrorHandler
+  };
+  Webpack(config, function(err, stats) {
+
+      if(err) return handlers.onFatalError(err, 'Fatal error');
+      var jsonStats = stats.toJson();
+      if(!jsonStats) {
+
+      }
+      if(jsonStats.errors.length > 0) {
+        return handlers.onError(jsonStats.errors.toString(), 'Stats error');
+      }
+      if(jsonStats.warnings.length > 0) {
+        return handlers.onWarning(jsonStats.warnings.toString(), 'Stats warning');
+      }
+      done();
+
+
+    });
+}
--- gulp/tasks/examples.js
+++ gulp/tasks/examples.js
@@ -1,14 +1,46 @@
 var gulp        = require('gulp');
 var concat			= require('gulp-concat');
-var webpack   = require("./webpack");
+var webpack   = require("webpack");
+var bundle		= require('./bundle');
 var gutil     = require("gulp-util");
 var path = require("path");
 
-var webpackConfig = require("../../config/webpack.config.js");
+webpackConfig = {
+	entry: {
+		'index' : './examples/index.js',
+		'shared' : './examples/shared.js',
+		'examples' : './examples/examples.js'
+	},
+	output: {
+		path: path.join(__dirname, "../../examples/build"),
+		filename: "[name].js",
+		libraryTarget: "umd"
+	},
+	externals: {
+		"react/addons": {
+			root : 'React',
+			commonjs : 'react/addons',
+			commonjs2 : 'react/addons',
+			amd : 'react/addons'
+		},
+		"moment" : "moment"
+	},
+	module: {
+		loaders: [
+		{ test: /\.js$/, loader: 'jsx-loader?stripTypes&harmony' } // loaders can take parameters as a querystring
+		]
+	},
+	plugins: [
+	new webpack.optimize.DedupePlugin(),
 
+	new webpack.optimize.OccurenceOrderPlugin(),
+	new webpack.optimize.AggressiveMergingPlugin()
+	]
+}
 
 
 gulp.task('script-deps',  function() {
+
 	return gulp.src([
 		'node_modules/es5-shim/es5-shim.js'
 	])
@@ -33,18 +65,7 @@ gulp.task('styles', function () {
 
 
 gulp.task("examples", ['script-deps', 'copy-dist', 'styles'],  function(callback) {
-	//override config entry / output
-		webpackConfig.entry = {
-			'index' : './examples/index.js',
-			'shared' : './examples/shared.js',
-			'examples' : './examples/examples.js'
-		};
 
-		webpackConfig.output = {
-			path: path.join(__dirname, "../../examples/build"),
-			filename: "[name].js",
-			libraryTarget: "umd"
-		};
 		// run webpack
-		webpack(webpackConfig, callback);
+		bundle(Object.create(webpackConfig), callback);
 });
